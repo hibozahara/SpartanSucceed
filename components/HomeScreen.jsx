f36769020a375cef679fe23738f3c04e61fbf0f4
Hibo Osman <hibozahara@hibos-mbp.attlocal.net>
2020-08-19 20:22:04 -0700

    login page changes

--- components/HomeScreen.jsx
+++ components/HomeScreen.jsx
@@ -1,15 +1,58 @@
+
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Image, TouchableOpacity } from "react-native";
 import { NavigationContainer, StackActions } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { color } from "react-native-reanimated";
 
 export default function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "#567FED" }}>
+      <Image style={styles.logo} source={require('./images/ST.png')} />
+      <Text style={[styles.defaultStyle]} >Welcome to SpartanToots</Text>
 
-      <Button title="Log in" onPress={() => navigation.navigate("Log in")} />
+      <TouchableOpacity style={styles.buttonContainer} onPress={() => navigation.navigate("Log in")}>
+        <Text style={styles.buttonText}> LOGIN</Text></TouchableOpacity>
+
+      <TouchableOpacity style={styles.buttonContainer} onPress={() => navigation.navigate("")}>
+        <Text style={styles.buttonText}> SIGN UP</Text></TouchableOpacity>
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  defaultStyle: {
+    fontFamily: "GillSans-Bold",
+    fontSize: 35,
+    color: "#FFF",
+    marginBottom: 75
+  },
+
+  logo: {
+    width: 200,
+    height: 200,
+    marginTop: 0,
+    marginBottom: 40,
+    marginTop: -170
+  },
+
+  buttonContainer: {
+    marginTop: 0,
+    marginBottom: 25,
+    width: 190,
+    backgroundColor: '#fff',
+    paddingVertical: 20,
+    borderRadius:10,
+    
+  },
+
+  buttonText: {
+    textAlign: 'center',
+    color: '#567FED',
+    fontWeight: '900',
+   
+  }
+
+});
--- components/LogIn.jsx
+++ components/LogIn.jsx
@@ -1,9 +1,80 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Image, TextInput } from "react-native";
 import { NavigationContainer, StackActions } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+
 export default function LogIn() {
-  return <Text>Log In Page</Text>;
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "#567FED" }}>
+      <Image style={styles.logo} source={require('./images/ST.png')} />
+      <Text style={[styles.defaultStyle]} >Login</Text>
+
+
+      <TextInput placeholder="Username" placeholderTextColor='rgba(0,0,0,0.5)' style={styles.input}/>
+
+      <TextInput placeholder="Password" placeholderTextColor='rgba(0,0,0,0.5)' style={styles.input}/>
+
+      <TouchableOpacity style={styles.buttonContainer} onPress={() => navigation.navigate("Log in")}>
+        <Text style={styles.buttonText}> LOGIN</Text></TouchableOpacity>
+
+    
+
+    </View>
+  );
 }
+
+
+
+
+
+const styles = StyleSheet.create({
+  defaultStyle: {
+    fontFamily: "GillSans-Bold",
+    fontSize: 35,
+    color: "#FFF",
+    marginBottom: 75
+  },
+
+  logo: {
+    width: 200,
+    height: 200,
+    marginTop: 0,
+    marginBottom: 40,
+    marginTop: -170
+  },
+
+  buttonContainer: {
+    marginTop: 0,
+    marginBottom: 25,
+    width: 190,
+    backgroundColor: '#fff',
+    paddingVertical: 20,
+    borderRadius:10
+
+  },
+
+  buttonText: {
+    textAlign: 'center',
+    color: '#567FED',
+    fontWeight: '900',
+
+  },
+
+  input: {
+
+    height: 40,
+    borderWidth: 3.5,
+    borderColor: '#339CFF',
+    backgroundColor: 'rgba(255,255,255,1.3)',
+    marginTop: 15,
+    marginBottom: 30,
+    color: '#FFF',
+    paddingHorizontal: 100,
+    textAlignVertical: 'top',
+    borderRadius:10,
+},
+
+
+});
