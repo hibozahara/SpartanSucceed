ebffd153b0c937d47a06ab05361155e8f6f335c0
saoudx <saoud.basedeq@sjsu.edu>
2020-09-07 20:45:43 -0700

    adding screens folder

--- App.js
+++ App.js
@@ -3,8 +3,9 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import LogIn from "./components/LogIn";
-import HomeScreen from "./components/HomeScreen";
+import LogIn from "./components/screens/LogIn";
+import HomeScreen from "./components/screens/HomeScreen";
+import HomeHome from "./components/screens/HomeHome";
 
 const Stack = createStackNavigator();
 
@@ -14,6 +15,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="HomeScreen">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Log in" component={LogIn} />
+        <Stack.Screen name="Home Home" component={HomeHome} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ components/screens/HomeHome.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const HomeHome = () => {
+  return (
+    <View>
+      <Text>HOME HOME PAGE</Text>
+    </View>
+  );
+};
+
+export default HomeHome;
